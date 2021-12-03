746bacfd52c94d587c6f4ef8916e7afec73535a1
Miguel <soccerpoof013@gmail.com>
2021-12-03 13:43:55 -0600

    Update Maquina_De_Turing.py

--- Maquina_De_Turing.py
+++ Maquina_De_Turing.py
@@ -8,14 +8,9 @@ Created on Thu Dec  2 14:20:44 2021
 alfabeto=["a",'b','c']
 entrada="AB"
 cintaInput=list(entrada)
-#cintaInput.append(" ")
-#cintaInput.insert(0, " ")
-
-
-
-
-if len(cintaInput) == 0:
-    print(Exception("La entrada esta vacia"))
+cintaInput.append(" ")
+cintaInput.insert(0, " ")
+estados=[]
 
 class regla:
     def __init__(self, lectura, escritura, movimiento, estadoDestino):
@@ -28,18 +23,15 @@ class estado:
     def __init__(self):
         self.reglas=[]
         self.numeroRegla=[]
-        self.estadosDestino=[]
         
     def agregarRegla(self, lectura, escritura, movimiento, estadoDestino):
         regla1 = regla(lectura, escritura, movimiento, estadoDestino)
         self.reglas.append(regla1)
         self.numeroRegla.append(lectura)
-        self.estadosDestino = estadoDestino
         
     def movimiento(self, cinta, indice):
         numeroDeregla = self.numeroRegla.index(cinta[indice])
         regla = self.reglas.__getitem__(numeroDeregla)
-        print(numeroDeregla)
         
         if regla.lectura == cinta[indice]:
             cinta[indice] = regla.escritura
@@ -63,61 +55,70 @@ class estado:
         elif regla.movimiento == "S":
             indice = indice
         
-        print(indice, cinta)
-        regla.estadoDestino.movimiento(cinta,indice)
+        if regla.estadoDestino != 0:
+            print(cinta)
+            regla.estadoDestino.movimiento(cinta,indice)
+        else:
+            print("CINTA DE SALIDA:",cinta)
 
-q0=estado()
-q1=estado()
-q2=estado()
-q3=estado()
-q4=estado()
-q5=estado()
-q6=estado()
-q7=estado()
-q8=estado()
-q9=estado()
-q10=estado()
-q11=estado()
-q12=estado()
-q13=estado()
-q14=estado()
-q15=estado()
-q16=estado()
-q17=estado()
+def main(cintaInput):    
+    if len(cintaInput) == 0:
+        print(Exception("La entrada esta vacia"))
+    else:
+        print("CINTA DE ENTRADA:",cintaInput,"\n")
+        for i in range(0,18):
+            q0 = estado()
+            estados.append(q0)
+    
+    #REGLAS ESTADO 0
+    estados[0].agregarRegla('A','A','R',estados[1])
+    estados[0].agregarRegla('B','B','R',estados[1])
+    estados[0].agregarRegla('C','C','R',estados[1])
+    
+    #REGLAS ESTADO 1
+    estados[1].agregarRegla('A','A','A',estados[1])
+    estados[1].agregarRegla('B','B','R',estados[1])
+    estados[1].agregarRegla('C','C','R',estados[1])
+    estados[1].agregarRegla(' ','Y','L',estados[1])
+    
+    #REGLAS ESTADO 2
+    estados[2].agregarRegla('A','A','L',estados[2])
+    estados[2].agregarRegla('B','B','L',estados[2])
+    estados[2].agregarRegla('C','C','L',estados[2])
+    estados[2].agregarRegla(' ',' ','R',estados[3])
+    
+    #REGLAS ESTADO 3    
+    estados[3].agregarRegla('A','A','R',estados[4])
+    estados[3].agregarRegla('B','B','R',estados[4])
+    estados[3].agregarRegla('C','C','R',estados[4])
+    
+    #REGLAS ESTADO 4
+    estados[4].agregarRegla('A','X','R',estados[5])
+    estados[4].agregarRegla('B','X','R',estados[6])
+    estados[4].agregarRegla('C','X','R',estados[7])
+    estados[4].agregarRegla('Y','Y','L',estados[11])
+    
+    #REGLAS ESTADO 5
+    estados[5].agregarRegla('A','A','R',estados[8])
+    estados[5].agregarRegla('B','B','R',estados[8])
+    estados[5].agregarRegla('C','C','R',estados[8])
+    estados[5].agregarRegla('Y','Y','R',estados[8])
+    estados[5].agregarRegla(' ','A','L',estados[8]) 
+    
+    #REGLAS ESTADO 6
+    estados[6].agregarRegla('A','A','R',estados[9])
+    estados[6].agregarRegla('B','B','R',estados[9])
+    estados[6].agregarRegla('C','C','R',estados[9])
+    estados[6].agregarRegla('Y','Y','R',estados[9])
+    estados[6].agregarRegla(' ','B','L',estados[9]) 
+    
+    #INICIO DE LAS TRANCISIONES
+    estados[0].movimiento(cintaInput, 0)
 
-q0.agregarRegla('C','C','R',q1)
-q0.agregarRegla('A','YY','R',q1)
-q1.agregarRegla('B','zzz','R',q1)
-
-q0.movimiento(cintaInput, 0)
-print(cintaInput)
+main(cintaInput)
 
 
-"""
 
-    def agregarDestinos(self, estadosDestino):
-        self.estadosDestino = estadosDestino
-        
-q0.agregarDestinos([q1])
-q1.agregarDestinos([q1,q2])
-q2.agregarDestinos([q2,q3])
-q3.agregarDestinos([q4])
-q4.agregarDestinos([q5,q7,q11])
-q5.agregarDestinos([q5,q8])
-q6.agregarDestinos([q6,q9])
-q7.agregarDestinos([q7,q10])
-q8.agregarDestinos([q4,q8])
-q9.agregarDestinos([q4,q9])
-q10.agregarDestinos([q4,q10])
-q11.agregarDestinos([q11,q12])
-q12.agregarDestinos([q13,q14,q15])
-q13.agregarDestinos([q13,q16])
-q14.agregarDestinos([q14,q16])
-q15.agregarDestinos([q15,q16])
-q16.agregarDestinos(q16,q17)
-
-
-"""
 
 
 
