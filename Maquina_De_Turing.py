3f0f1c9cde4f20283171639755626b58233b34f1
Miguel <soccerpoof013@gmail.com>
2021-12-02 19:20:36 -0600

    Update Maquina_De_Turing.py

--- Maquina_De_Turing.py
+++ Maquina_De_Turing.py
@@ -17,10 +17,11 @@ if len(cintaInput) == 0:
     print(Exception("La entrada esta vacia"))
 
 class regla:
-    def __init__(self, lectura, escritura, movimiento):
+    def __init__(self, lectura, escritura, movimiento, estadoDestino):
         self.lectura = lectura
         self.escritura = escritura
         self.movimiento = movimiento
+        self.estadoDestino = estadoDestino
         
 class estado:
     def __init__(self):
@@ -28,17 +29,34 @@ class estado:
         self.numeroRegla=[]
         self.estadosDestino=[]
         
-    def agregarRegla(self, lectura, escritura, movimiento):
-        regla1 = regla(lectura, escritura, movimiento)
+    def agregarRegla(self, lectura, escritura, movimiento, estadoDestino):
+        regla1 = regla(lectura, escritura, movimiento, estadoDestino)
         self.reglas.append(regla1)
         self.numeroRegla.append(lectura)
         
     def movimiento(self, cinta, indice):
         numeroDeregla = self.numeroRegla.index(cinta[indice])
         regla = self.reglas.__getitem__(numeroDeregla)
-    
-    def agregarDestinos(self, estadosDestino):
-        self.estadosDestino = estadosDestino
+        
+        if regla.lectura == cinta[indice]:
+            cinta[indice] = regla.escritura
+        
+        if regla.movimiento == "R":
+            indice = indice+1
+        elif regla.movimiento == "L":
+            if indice-1 < 0:
+                cinta.insert(0," ")
+                indice = indice-1
+            else:
+                indice = indice-1
+        elif regla.movimiento == "S":
+            if indice+1 > len(cinta):
+                cinta.append(" ")
+                indice = indice+1
+            else:
+                indice = indice+1
+            indice = indice
+
 
 q0=estado()
 q1=estado()
@@ -60,11 +78,33 @@ q16=estado()
 q17=estado()
 
 
+
+
+"""
+
+    def agregarDestinos(self, estadosDestino):
+        self.estadosDestino = estadosDestino
+        
 q0.agregarDestinos([q1])
 q1.agregarDestinos([q1,q2])
+q2.agregarDestinos([q2,q3])
+q3.agregarDestinos([q4])
+q4.agregarDestinos([q5,q7,q11])
+q5.agregarDestinos([q5,q8])
+q6.agregarDestinos([q6,q9])
+q7.agregarDestinos([q7,q10])
+q8.agregarDestinos([q4,q8])
+q9.agregarDestinos([q4,q9])
+q10.agregarDestinos([q4,q10])
+q11.agregarDestinos([q11,q12])
+q12.agregarDestinos([q13,q14,q15])
+q13.agregarDestinos([q13,q16])
+q14.agregarDestinos([q14,q16])
+q15.agregarDestinos([q15,q16])
+q16.agregarDestinos(q16,q17)
 
 
-
+"""
 
 
 
